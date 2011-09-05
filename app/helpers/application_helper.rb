b8a47dadbe071f8fcc31c0be06c2990c277ecba4
Mario Apitz <mario@zwergenkrieger.de>
2011-09-05 21:44:05 +0200

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   
+  # Logo helper
+  def logo
+    base_logo =  image_tag("logo.png", :alt => "Sample App", :class => "round")
+    base_logo
+  end
+  
   # Return a title on per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
